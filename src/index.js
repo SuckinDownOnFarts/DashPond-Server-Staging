9f1b3029156e44741e85c26aaa92c1e0280acd0c
Danny <domezero@gmail.com>
2023-01-26 22:12:55 -0600

    Prepare for deployement

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './Context/AuthProvider';
 import { registerLicense } from '@syncfusion/ej2-base';
 import './index.css';
 
 //SyncFusion license key
-registerLicense('ORg4AjUWIQA/Gnt2VVhjQlFaclhJXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxRd0dhWX9acndRRWZcUEE=')
+registerLicense('ORg4AjUWIQA/Gnt2VVhjQlFaclhJXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxRd0dhWX9acndRRWZcUEE=');
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

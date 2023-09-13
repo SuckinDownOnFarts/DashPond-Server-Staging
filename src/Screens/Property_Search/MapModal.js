aa08bfd0fb54592d959039844264f74ec7a113e9
SuckingDownOnFarts <daniel@colosino.com>
2023-09-13 00:19:02 -0500

    test new basemap layer

--- src/Screens/Property_Search/MapModal.js
+++ src/Screens/Property_Search/MapModal.js
@@ -44,7 +44,7 @@ const MapModal = () => {
                 <MapContainer center={center} zoom={13}>
                     <TileLayer
                         attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-                        url='https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png'
+                        url='http://services.arcgisonline.com/arcgis/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}'
                     />
 
                     <Marker
